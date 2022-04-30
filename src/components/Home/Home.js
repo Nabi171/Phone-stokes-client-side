daf1683503737e641ca98fc56cb5debaf6c24623
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-04-30 18:20:21 +0600

    home page updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,16 @@ import TopBanner from '../TopBanner/TopBanner';
 import Inventory from '../Inventory/Inventory';
 import Footer from '../Footer/Footer';
 import AdminPanel from '../AdminPanel/AdminPanel';
+import Instructions from '../Instructions/Instructions';
 
 const Home = () => {
     return (
         <div>
             <TopBanner></TopBanner>
+            <br />
+            <h1 className='text-center text-white'>Instructions</h1>
+            <hr className='mx-auto w-25 text-white' />
+            <Instructions></Instructions>
             <br />
             <h1 className='text-center text-white'>Collection</h1>
             <hr className='mx-auto w-25 text-white' />
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -2,12 +2,12 @@ import React from 'react';
 import img from '../Instructions/instructions.png'
 const Instructions = () => {
     return (
-        <div>
-            <div className='row align-items-center'>
+        <div className='container'>
+            <div className='row align-items-center justify-content-between'>
                 <div className='col-lg-6 col-sm-12 col-md-6'>
-                    <img src={img} className='w-50 p-4' alt="" />
+                    <img src={img} className='w-100 p-4' alt="" />
                 </div>
-                <div className='col-lg-6 col-sm-12 col-md-6'>
+                <div className='col-lg-6 col-sm-12 col-md-6 text-white shadow'>
                     <h6>Condition for Stokes</h6>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto rerum magni repellendus consectetur sequi perferendis placeat possimus enim in animi fuga ullam, nemo voluptas ex adipisci incidunt corporis minima officiis?</p>
                 </div>
